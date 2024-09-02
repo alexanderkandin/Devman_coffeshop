bff2433b18a6fc5f966769959ecd9fcaaf6a54d0
alexanderkandin <46374639+alexanderkandin@users.noreply.github.com>
2024-09-02 15:28:24 +0300

    Рабочий код

--- .venv/main.py
+++ .venv/main.py
@@ -5,7 +5,9 @@ from geopy import distance
 from pprint import pprint
 from flask import Flask
 
+
 apikey = 'b222220d-81ea-4425-bff5-8f6f8fe47f65'
+
 
 def fetch_coordinates(apikey, address):
     base_url = "https://geocode-maps.yandex.ru/1.x"
@@ -25,59 +27,58 @@ def fetch_coordinates(apikey, address):
     return lon, lat
 
 
-location = input('Укажите город: ')
-location_x,location_y = fetch_coordinates(apikey,location)
-your_location = (location_x,location_y)
-
-
-with open('coffee.json', 'r', encoding='CP1251') as my_file:
-    file_contents = json.loads(my_file.read())
-
-
-coffeeshop = []
-
-
-for index, coffee_shop in enumerate(file_contents, start=1):
-    coffe_name = coffee_shop['Name']
-    coffe_coordinates = coffee_shop['geoData']['coordinates']
-    your_distance = (distance.distance(your_location, coffe_coordinates).km)
-    coffe = dict()
-    coffe['title'] = coffe_name
-    coffe['distance'] = your_distance
-    coffe['latitude'] = coffe_coordinates[1]
-    coffe['longitude'] = coffe_coordinates[0]
-    coffeeshop.append(coffe)
-
-
-def get_coffe_dist(coffeeshop):
-    return coffeeshop['distance']
-
-
-five_coffeeshop = sorted(coffeeshop,key=get_coffe_dist)[:6]
-
-
-m = folium.Map([55.75426247479371, 37.620765484236614], zoom_start=12)
-
-
-for i in five_coffeeshop:
-    folium.Marker(
-        location= [i['latitude'], i['longitude']],
-        tooltip="Click me!",
-        popup="Mt. Hood Meadows",
-        icon=folium.Icon(icon="cloud"),
-    ).add_to(m)
-
-
-m.save('map.html')
-
-
 def coffeeshop_map():
     with open('map.html') as file:
       return file.read()
 
-app = Flask(__name__)
-app.add_url_rule('/', 'hello', coffeeshop_map)
-app.run('0.0.0.0')
+
+def main():
+    with open('coffee.json', 'r', encoding='CP1251') as my_file:
+        file_contents = json.loads(my_file.read())
+
+    location = input('Укажите локацию: ')
+    location_x,location_y = fetch_coordinates(apikey,location)
+    your_location = (location_x,location_y)
+
+    coffeeshop = []
+    for coffee_shop in file_contents:
+        coffe_name = coffee_shop['Name']
+        coffe_coordinates = coffee_shop['geoData']['coordinates']
+        your_distance = (distance.distance(your_location, coffe_coordinates).km)
+        coffe = dict()
+        coffe['title'] = coffe_name
+        coffe['distance'] = your_distance
+        coffe['latitude'] = coffe_coordinates[1]
+        coffe['longitude'] = coffe_coordinates[0]
+        coffeeshop.append(coffe)
+
+
+    def get_coffe_dist(coffeeshop):
+        return coffeeshop['distance']
+
+
+    five_coffeeshop = sorted(coffeeshop,key=get_coffe_dist)[:6]
+    m = folium.Map([55.75426247479371, 37.620765484236614], zoom_start=12)
+
+
+    for i in five_coffeeshop:
+        folium.Marker(
+            location= [i['latitude'], i['longitude']],
+            tooltip="Click me!",
+            popup="Mt. Hood Meadows",
+            icon=folium.Icon(icon="cloud"),
+        ).add_to(m)
+
+
+    m.save('map.html')
+
+    app = Flask(__name__)
+    app.add_url_rule('/', 'hello', coffeeshop_map)
+    app.run('0.0.0.0')
+
+
+if __name__ == "__main__":
+    main()
 
 
 
